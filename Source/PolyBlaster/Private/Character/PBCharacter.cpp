ab393309c1bf84a88fead55a426988ae8c8ce49f
Teguh <danielsxiong@gmail.com>
2022-04-23 22:43:35 +0700

    61. Net update frequency

--- Source/PolyBlaster/Private/Character/PBCharacter.cpp
+++ Source/PolyBlaster/Private/Character/PBCharacter.cpp
@@ -41,6 +41,8 @@ APBCharacter::APBCharacter()
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
 
 	TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+	NetUpdateFrequency = 66.f;
+	MinNetUpdateFrequency = 33.f;
 }
 
 void APBCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
