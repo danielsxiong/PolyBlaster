1118dcf8f93c3026517d7a7e3530b921ab5c8163
Teguh <danielsxiong@gmail.com>
2022-04-24 17:40:31 +0700

    66. Crouch footstep sounds and Projectile weapon class

---
+++ Source/PolyBlaster/Public/Weapon/Projectile.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class POLYBLASTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectile();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* CollisionBox;
+
+};
