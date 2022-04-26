af1f33aba0afc811fbbbd32e6bd8b81fe37a9f72
Teguh <danielsxiong@gmail.com>
2022-04-26 19:17:50 +0700

    73. Projectile tracer

--- Source/PolyBlaster/Public/Weapon/Projectile.h
+++ Source/PolyBlaster/Public/Weapon/Projectile.h
@@ -30,4 +30,9 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	class UProjectileMovementComponent* ProjectileMovementComponent;
 
+	UPROPERTY(EditAnywhere)
+	class UParticleSystem* Tracer;
+
+	class UParticleSystemComponent* TracerComponent;
+
 };
