9bba128bd1c44ff3a9c97bf0fce5fc26069fa506
Teguh <danielsxiong@gmail.com>
2022-07-04 19:08:31 +0700

    158. Pickup Class

---
+++ Source/PolyBlaster/Public/Pickups/Pickup.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class POLYBLASTER_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	APickup();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Sweep);
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* PickupSound;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+
+public:		
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Destroyed() override;
+
+};
