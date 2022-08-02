c0723a78a523418579bffb279023f36ab0555890
Teguh <danielsxiong@gmail.com>
2022-08-02 21:22:56 +0700

    178. Show the widget locally

--- Source/PolyBlaster/Private/Weapon/Weapon.cpp
+++ Source/PolyBlaster/Private/Weapon/Weapon.cpp
@@ -45,13 +45,10 @@ void AWeapon::BeginPlay()
 	Super::BeginPlay();
 	
 	// Only set collision enabled on server
-	if (HasAuthority())
-	{
-		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
-		AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
-	}
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
 
 	ShowPickupWidget(false);
 }
