0c2b7d0c5de61520ea147ccdaaa5106098b0f001
Teguh <danielsxiong@gmail.com>
2022-11-27 15:42:12 +0700

    224. Team Colors

---
+++ Source/PolyBlaster/Public/GameMode/PBTeamsGameMode.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/PBGameMode.h"
+#include "PBTeamsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLYBLASTER_API APBTeamsGameMode : public APBGameMode
+{
+	GENERATED_BODY()
+
+public:
+
+	// For players joining mid game
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	// For players leaving mid game
+	virtual void Logout(AController* Exiting) override;
+
+protected:
+
+	virtual void HandleMatchHasStarted() override;
+	
+};
